0eed00aff8cc0aa84f88707132cdfe86722b71b0
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-12-01 09:29:06 +0000

    Fix some tiny things

--- projects/backend/src/utils/prisma.ts
+++ projects/backend/src/utils/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client';
 
-// eslint-disable-next-line import/prefer-default-export
 export const prisma = new PrismaClient();
 export * from '@prisma/client';
