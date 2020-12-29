4ac315d54b9918c863c0631e6046bfdf658bbdab
Silas Rosenkranz <silas.rosenkranz@student.uni-siegen.de>
2020-12-29 10:11:49 +0000

    Refactor css vars

--- projects/frontend/src/theme.ts
+++ projects/frontend/src/theme.ts
@@ -11,30 +11,30 @@ const colors = {
   '--color-feedbax-primary': '#3a5568',
   '--color-feedbax-secondary': '#ff7d65',
 
-  '--black-100': shade(1.00, '#fff'),
-  '--black-95': shade(0.95, '#fff'),
-  '--black-90': shade(0.90, '#fff'),
-  '--black-85': shade(0.85, '#fff'),
-  '--black-80': shade(0.80, '#fff'),
+  '--color-black-100': shade(1.00, '#fff'),
+  '--color-black-95': shade(0.95, '#fff'),
+  '--color-black-90': shade(0.90, '#fff'),
+  '--color-black-85': shade(0.85, '#fff'),
+  '--color-black-80': shade(0.80, '#fff'),
 
-  '--white-100': tint(1.00, '#000'),
-  '--white-95': tint(0.95, '#000'),
-  '--white-90': tint(0.90, '#000'),
-  '--white-85': tint(0.85, '#000'),
-  '--white-80': tint(0.80, '#000'),
+  '--color-white-100': tint(1.00, '#000'),
+  '--color-white-95': tint(0.95, '#000'),
+  '--color-white-90': tint(0.90, '#000'),
+  '--color-white-85': tint(0.85, '#000'),
+  '--color-white-80': tint(0.80, '#000'),
 
-  '--transparent': [
+  '--color-transparent': [
     '#ffffff00',
     'rgba(0, 0, 0, 0)',
     'transparent',
   ],
 
   get '--color-primary-text' () {
-    return 'var(--white-100)';
+    return 'var(--color-white-100)';
   },
 
   get '--color-background-footer' () {
-    return 'var(--black-90)';
+    return 'var(--color-black-90)';
   },
 };
 
