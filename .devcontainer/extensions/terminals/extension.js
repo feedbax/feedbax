f44bae3e2efb406f417cf36024ae8d8a8147c27e
hedwiggggg <silas@rosenkra.nz>
2021-05-08 17:06:59 +0000

    refactor: - add feedbax.terminals.reset - add feedbax.terminals.open - remove: onStartupFinished

--- .devcontainer/extensions/terminals/extension.js
+++ .devcontainer/extensions/terminals/extension.js
@@ -4,39 +4,50 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	const { terminals } = vscode.window;
-	const terminalsToOpen = {
-		'@feedbax/frontend': '/workspace/feedbax/backend/packages/frontend',
-		'@feedbax/server': '/workspace/feedbax/backend/packages/server',
-		'@feedbax/api': '/workspace/feedbax/backend/packages/api',
-		'@feedbax/prisma': '/workspace/feedbax/backend/packages/prisma',
-		'@feedbax/common': '/workspace/feedbax/backend/packages/common',
+	const closeAllTerminals = () => {
+		const { terminals } = vscode.window;
+		
+		for (let i = 0; i < terminals.length; i += 1) {
+			const terminal = terminals[i];
+			terminal.dispose();
+		}
 	};
 
-	vscode.window.onDidOpenTerminal((terminal) => {
-		if (terminal.name === '') terminal.dispose();
-	});
+	const openTerminals = () => {
+		const terminalsToOpen = {
+			'@feedbax/frontend': '/workspace/feedbax/backend/packages/frontend',
+			'@feedbax/server': '/workspace/feedbax/backend/packages/server',
+			'@feedbax/api': '/workspace/feedbax/backend/packages/api',
+			'@feedbax/prisma': '/workspace/feedbax/backend/packages/prisma',
+			'@feedbax/common': '/workspace/feedbax/backend/packages/common',
+		};
 
-	for (let i = 0; i < terminals.length; i += 1) {
-		const terminal = terminals[i];
+		const terminalsEntries = Object.entries(terminalsToOpen);
 
-		if (terminal.name in terminalsToOpen) {
-			delete terminalsToOpen[terminal.name];
-		}
-	}
+		for (let i = 0; i < terminalsEntries.length; i += 1) {
+			const [name, path] = terminalsEntries[i];
+			
+			const terminal = vscode.window.createTerminal(name);
+	
+			terminal.name = name;
+			terminal.creationOptions.name = name;
 
-	const terminalsEntries = Object.entries(terminalsToOpen);
+			terminal.sendText(`cd ${path}`);
+			terminal.sendText(`export PS1='\\u${name}: '`);
+			terminal.sendText("clear");
+			terminal.show();	
+		}	
+	};
 
-	for (let i = 0; i < terminalsEntries.length; i += 1) {
-		const [name, path] = terminalsEntries[i];
-		
-		const terminal = vscode.window.createTerminal(name);
+	const resetTerminals = () => {
+		closeAllTerminals();
+		openTerminals();
+	};
 
-		terminal.sendText(`cd ${path}`);
-		terminal.sendText(`export PS1='\\u${name}: '`);
-		terminal.sendText("clear");
-		terminal.show();	
-	}
+	context.subscriptions.push(
+		vscode.commands.registerCommand('feedbax.terminals.open', () => openTerminals()),
+		vscode.commands.registerCommand('feedbax.terminals.reset', () => resetTerminals())
+	);
 }
 
 // eslint-disable-next-line no-undef
